f41094f8f976219ee3fc68662657d898d6e40390
Danil <dabilsch60@gmail.com>
2024-06-30 22:20:41 +0300

    update all method

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,6 @@
 package org.example;
 
 import java.util.*;
-import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
@@ -12,22 +11,35 @@ public class Main {
 
     public static void main(String[] args) {
 
+        String[] testData = new String[] {"anna", "iavn", "danil", "igory", "yra"};
+        String[] testDataInt = new String[] {"1, 2, 0", "4, 5"};
+        String[] testDataToZip = new String[] {"1","1","1", "1", "1", "1"};
+        String[] testDataToZip2 = new String[] {"2","2","2","2","2"};
+
+
+        System.out.println("countName(testData) = " + (countName(testData)));
+        System.out.println("sorterString(testData) = " + sorterString(testData));
+        sortedInt(testDataToZip);
+        // random(25214903917L, 11,  Math.pow(2, 48)).forEach(System.out :: println);
+        zip(stream(testDataToZip), stream(testDataToZip2)).forEach(System.out ::println);
+
+
     }
-    public static String[] countName(String[] array){
+    public static String countName(String[] array){
 
         String[] result = IntStream.range(0, array.length)
                 .filter(i -> i % 2 != 0)
                 .mapToObj(i -> i + ". " + array[i])
-                .toArray(String[]::new);
+                .toArray(String[] :: new);
 
-        return result;
+        return stream(result).collect(Collectors.joining(", "));
     }
     public static List<String> sorterString(String[] name){
         List<String> list = List.of(name);
        List<String> result = list.stream()
                 .sorted(Comparator.reverseOrder())
+               .map(String :: toUpperCase)
                 .collect(Collectors.toList());
-
         return result;
     }
     public static void sortedInt(String[] name){
@@ -38,20 +50,22 @@ public class Main {
                 .map(String::trim)
                 .sorted()
                 .toList();
-        System.out.println(listExtracted);
+        System.out.println(listExtracted.stream().collect(Collectors.joining(", ")));
     }
-    public static Stream<Long> random(Long a, int c, Long m){
-        return Stream.iterate(25L, n -> (a * n + c) % m);
+    public static Stream<Long> random(Long a, int c, double m){
+        return Stream.iterate(25L, n -> (long) ((a * n + c) % m));
     }
-    public static <T> Stream<T> zip(Stream<T> first, Stream<T> second){
+    public static <T> Stream<T> zip(Stream<T> first, Stream<T> second) {
+        List<T> list1 = first.toList();
+        List<T> list2 = second.toList();
 
-        Iterator<String> iterator = (Iterator<String>) first.iterator();
-        Iterator<String> iterator1 = (Iterator<String>) second.iterator();
-        ArrayList<String> arrayList = new ArrayList<>();
-        while (iterator1.hasNext() && iterator.hasNext()){
-            arrayList.add(iterator.next() + " " + iterator1.next());
-        }
-        return (Stream<T>) arrayList.stream();
+        Iterator<T> iterator = list1.iterator();
+        Iterator<T> iterator1 = list2.iterator();
+            String[] arr = IntStream.range(0, Math.min(list1.size(), list2.size()))
+                    .mapToObj(i -> iterator.next() + ", " + iterator1.next())
+                    .toArray(String[]::new);
+        return (Stream<T>) stream(arr);
     }
+
 
 }
