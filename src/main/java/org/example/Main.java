59517460400ba4ebf61dc39f929e363162eff737
Danil <dabilsch60@gmail.com>
2024-07-02 22:27:34 +0300

    Changed metod zip

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -17,40 +17,39 @@ public class Main {
         String[] testDataToZip2 = new String[] {"2","2","2","2","2"};
 
 
-        System.out.println("countName(testData) = " + (countName(testData)));
-        System.out.println("sorterString(testData) = " + sorterString(testData));
-        sortedInt(testDataToZip);
+      //  System.out.println("countName(testData) = " + (countName(testData)));
+      //  System.out.println("sorterString(testData) = " + sorterString(testData));
+      //  sortedInt(testDataToZip);
         // random(25214903917L, 11,  Math.pow(2, 48)).forEach(System.out :: println);
-        zip(stream(testDataToZip), stream(testDataToZip2)).forEach(System.out ::println);
+
+        var first = Stream.of(new User("A"), new User("B"), new User("C"));
+        var second = Stream.of(new User("1"), new User("2"), new User("3"));
+        zip(first, second).forEach(System.out ::println);
 
 
     }
     public static String countName(String[] array){
 
-        String[] result = IntStream.range(0, array.length)
+        return IntStream.range(0, array.length)
                 .filter(i -> i % 2 != 0)
                 .mapToObj(i -> i + ". " + array[i])
-                .toArray(String[] :: new);
-
-        return stream(result).collect(Collectors.joining(", "));
+                .collect(Collectors.joining(", "));
     }
     public static List<String> sorterString(String[] name){
         List<String> list = List.of(name);
-       List<String> result = list.stream()
+        return list.stream()
                 .sorted(Comparator.reverseOrder())
-               .map(String :: toUpperCase)
+                .map(String :: toUpperCase)
                 .collect(Collectors.toList());
-        return result;
     }
     public static void sortedInt(String[] name){
         List<String> list = List.of(name);
-        List<String> listExtracted = list.stream()
+        System.out.println(list.stream()
                 .map(x -> x.split(","))
                 .flatMap(Arrays::stream)
                 .map(String::trim)
                 .sorted()
-                .toList();
-        System.out.println(listExtracted.stream().collect(Collectors.joining(", ")));
+                .collect(Collectors.joining(", ")));
     }
     public static Stream<Long> random(Long a, int c, double m){
         return Stream.iterate(25L, n -> (long) ((a * n + c) % m));
@@ -58,14 +57,22 @@ public class Main {
     public static <T> Stream<T> zip(Stream<T> first, Stream<T> second) {
         List<T> list1 = first.toList();
         List<T> list2 = second.toList();
-
         Iterator<T> iterator = list1.iterator();
         Iterator<T> iterator1 = list2.iterator();
-            String[] arr = IntStream.range(0, Math.min(list1.size(), list2.size()))
-                    .mapToObj(i -> iterator.next() + ", " + iterator1.next())
-                    .toArray(String[]::new);
-        return (Stream<T>) stream(arr);
+
+        return IntStream.range(0, Math.min(list1.size(), list2.size()))
+                .mapToObj(i -> Stream.of(iterator.next(), iterator1.next()))
+                .flatMap(s -> s);
     }
+}
+class User{
+    String name;
 
-
+    public User(String name) {
+        this.name = name;
+    }
+    @Override
+    public String toString() {
+        return "name " + name;
+    }
 }
